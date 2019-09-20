a7ffd4671f688fdba8fe4b5f038e9c0c176dad46
Titus Ouko <titusouko@gmail.com>
2019-09-20 14:24:46 +0300

    Create quote component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  quotes:Quote[] =[
-   
- ]
+   {id:1, quote:'Mobile is the enabling centerpiece of digital convergence. Mobile is the glue for all other digital industries to use when approaching convergence, but mobile is also the digital gateway for the real world to join in this global metamorphosis of human behavior.'},
+
+ ];
 }
