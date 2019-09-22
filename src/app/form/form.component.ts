dfbc3a914fdd48e6db0e952ffddf123f0af02574
Titus Ouko <titusouko@gmail.com>
2019-09-22 15:24:46 +0300

    Impotr aoutput and event emitter

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -8,6 +9,11 @@ import { Quote } from '../quote';
 export class FormComponent implements OnInit {
 
   newQuote = new Quote(1,'', '', '', 0, 0, new Date());
+  @Output () addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+  }
 
   constructor() { }
 
