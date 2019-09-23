90cdd229cc1753ebbea71a4d8bc3c627383f5d7b
Titus Ouko <titusouko@gmail.com>
2019-09-23 15:51:09 +0300

    Add custom pipe for time

--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -1,28 +1,38 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'timeCount'
+  name: 'timeCount',
+  pure:true
 })
 export class TimeCountPipe implements PipeTransform {
 
-  transform(value: any): number {
-    let today:Date = new Date(); 
-    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    var dateDifference = Math.abs(value - todayWithNoTime) 
-    const secondsInDay = 86400; 
-    const minutesInDay = 1440;
-    const hoursInDay = 24;
-    var dateDifferenceSeconds = dateDifference*0.001; 
-    var minCounter = (dateDifferenceSeconds/60)/ minutesInDay;
-    var hourCounter = (dateDifferenceSeconds/3600)/hoursInDay;
-    var dateCounter = dateDifferenceSeconds/secondsInDay;
-
-    if (dateCounter=>1){
-      return dateCounter;
-    }else{
-      return 0
+  transform(value: any, args?: any): any {
+    if (value) {
+        const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+        if (seconds < 29) 
+            return 'Just now';
+        const intervals = {
+            'year': 31536000,
+            'month': 2592000,
+            'week': 604800,
+            'day': 86400,
+            'hour': 3600,
+            'minute': 60,
+            'second': 1
+        };
+        let counter;
+        for (const i in intervals) {
+            counter = Math.floor(seconds / intervals[i]);
+            if (counter > 0)
+                if (counter === 1) {
+                    return counter + ' ' + i + ' ago';
+                } else {
+                    return counter + ' ' + i + 's ago'; 
+                }
+        }
     }
-  }
-  }
+    return value;
+ }
+}
 
 
