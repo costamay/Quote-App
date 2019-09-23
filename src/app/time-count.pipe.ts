d57b2be8aaae0e357019bf783d67f33913bdc71a
Titus Ouko <titusouko@gmail.com>
2019-09-23 17:35:50 +0300

    Implement time pipe

--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -9,7 +9,7 @@ export class TimeCountPipe implements PipeTransform {
   transform(value: any, args?: any): any {
     if (value) {
         const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
-        if (seconds < 29) 
+        if (seconds < 29) // less than 30 seconds ago will show as 'Just now'
             return 'Just now';
         const intervals = {
             'year': 31536000,
@@ -25,14 +25,14 @@ export class TimeCountPipe implements PipeTransform {
             counter = Math.floor(seconds / intervals[i]);
             if (counter > 0)
                 if (counter === 1) {
-                    return counter + ' ' + i + ' ago';
+                    return counter + ' ' + i + ' ago'; // singular (1 day ago)
                 } else {
-                    return counter + ' ' + i + 's ago'; 
+                    return counter + ' ' + i + 's ago'; // plural (2 days ago)
                 }
         }
     }
     return value;
- }
+}
 }
 
 
